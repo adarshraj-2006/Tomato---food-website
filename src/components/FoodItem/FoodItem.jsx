ddc7033523f7b3e8866b04a5ae5d60598b94164f
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2026-01-12 20:49:30 +0530

    Add FoodDetail and Contact pages with styles and functionality

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,9 +1,8 @@
-import { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { StoreContext } from '../../context/StoreContext';
-import './FoodItem.css';
-import { assets } from '../../assets/assets';
-
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { StoreContext } from "../../context/StoreContext";
+import "./FoodItem.css";
+import { assets } from "../../assets/assets";
 
 const FoodItem = ({ id, name, price, description, image }) => {
   const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
@@ -11,43 +10,45 @@ const FoodItem = ({ id, name, price, description, image }) => {
   return (
     <div className="food-item">
 
-      {/* IMAGE (CLICKABLE) */}
+      {/* IMAGE → FOOD DETAIL */}
       <Link to={`/food/${id}`} className="food-link">
         <div className="food-item-img-container">
           <img className="food-item-image" src={image} alt={name} />
         </div>
       </Link>
 
-      {/* CART CONTROLS (NOT CLICKABLE) */}
-      {!cartItems[id] ? (
-        <img
-          className="add"
-          onClick={() => addToCart(id)}
-          src={assets.add_icon_white}
-          alt="Add item to cart"
-        />
-      ) : (
-        <div className="food-item-counter">
+      {/* CART ACTIONS (NO LINK WRAP) */}
+      <div className="food-item-cart">
+        {!cartItems[id] ? (
           <img
-            onClick={() => removeFromCart(id)}
-            src={assets.remove_icon_red}
-            alt="Remove item from cart"
-          />
-          <p>{cartItems[id]}</p>
-          <img
+            className="food-item-add"
             onClick={() => addToCart(id)}
-            src={assets.add_icon_green}
-            alt="Add more item to cart"
+            src={assets.add_icon_white}
+            alt="Add to cart"
           />
-        </div>
-      )}
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => removeFromCart(id)}
+              src={assets.remove_icon_red}
+              alt="Remove"
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              onClick={() => addToCart(id)}
+              src={assets.add_icon_green}
+              alt="Add more"
+            />
+          </div>
+        )}
+      </div>
 
-      {/* INFO (CLICKABLE) */}
+      {/* INFO → FOOD DETAIL */}
       <Link to={`/food/${id}`} className="food-link">
         <div className="food-item-info">
           <div className="food-item-name-rating">
             <p>{name}</p>
-            <img src={assets.rating_starts} alt="Rating stars" />
+            <img src={assets.rating_starts} alt="Rating" />
           </div>
           <p className="food-item-desc">{description}</p>
           <p className="food-item-price">${price}</p>
